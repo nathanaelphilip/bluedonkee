d5c9491cbb0010830d89e2262661fee97a33c4db
Nathanael Moody <hello@nathanaelphilip.com>
2020-03-20 11:21:16 -0500

    adds pagination to group categories

--- src/views/GroupCategory.vue
+++ src/views/GroupCategory.vue
@@ -1,47 +1,67 @@
 <template>
   <article v-if="!loading">
     <Intro :back="{ name: 'groups' }" :heading="`Category: ${category.fields.Name}`" />
-    <Groups :groups="groups" />
+    <Groups :groups="$store.getters['groups/getFetched'](id)" />
+    <Pager
+      @load="load"
+      :loading="$store.state.groups.loading === id"
+      v-if="$store.getters['groups/getOffset'](id)"
+    />
+    <BackTop v-if="!$store.getters['groups/getOffset'](id)" />
   </article>
 </template>
 
 <script>
 import {
-  getByIds,
   getBySlug
 } from '@/store/helpers'
 
+import BackTop from '@/components/molecules/BackTop'
 import Groups from '@/components/molecules/Groups'
 import Intro from '@/components/molecules/Intro'
+import Pager from '@/components/molecules/Pager'
+
+const pageSize = 20
 
 export default {
   name: 'group-category',
-  components: { Groups, Intro },
+  components: { BackTop, Groups, Intro, Pager },
 
   data () {
     return {
+      id: false,
       loading: true,
-      category: {},
-      groups: []
+      category: {}
     }
   },
 
   async mounted () {
+    this.id = `groups/categories/${this.$route.params.slug}`
+
     this.category = await getBySlug({
       slug: this.$route.params.slug,
       type: 'groupCategories'
     })
 
-    this.groups = this.category.fields.Groups ? await getByIds({
-      ids: this.category.fields.Groups,
-      type: 'groups'
-    }) : []
-
-    this.groups = this.groups.sort((a, b) => {
-      return a.fields.Name > b.fields.Name ? 1 : -1
-    })
+    if (!this.$store.getters['groups/getFetched'](this.id).length) {
+      await this.load()
+    }
 
     this.loading = false
+  },
+
+  methods: {
+    async load () {
+      await this.$store.dispatch('groups/fetch', {
+        id: this.id,
+        params: {
+          filterByFormula: `SEARCH("${this.category.fields.Name}", {Groups Categories})`,
+          pageSize,
+          sort: [{ field: 'Name', direction: 'asc' }],
+          offset: this.$store.getters['groups/getOffset'](this.id)
+        }
+      })
+    }
   }
 }
 </script>
