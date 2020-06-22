da07e2dcc0bd888dc64c923ecfc1538178f1255f
Nathanael Moody <hello@nathanaelphilip.com>
2020-06-22 10:02:41 -0500

    adds more styling for search

--- src/components/forms/Search.vue
+++ src/components/forms/Search.vue
@@ -1,12 +1,13 @@
 <template>
-  <form @submit.prevent class="form">
+  <form @submit.prevent class="form" :class="{'»open' : open}">
     <div class="box">
       <IconSearch width="16" height="16" />
-      <input v-model="input" type="text" class="input">
-      <button v-if="input" class="close">
+      <input @focus="open = true" @blur="open = false" v-model="input" type="text" class="input">
+      <button class="close">
         <IconClose height="9" width="9" />
       </button>
     </div>
+    <div class="results"></div>
   </form>
 </template>
 
@@ -19,6 +20,7 @@ export default {
 
   data () {
     return {
+      open: false,
       input: ''
     }
   }
@@ -26,11 +28,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .form {
+    position: relative;
+  }
+
   .box {
     @include Flex;
     border: 1px solid $GREY;
     border-radius: 4px;
     padding: grid(1) grid(3);
+
+    .»open & {
+      border-bottom-left-radius: 0;
+      border-bottom-right-radius: 0;
+    }
   }
 
   .input {
@@ -42,11 +53,34 @@ export default {
   }
 
   .close {
+    @include Hide;
     @include Flex ($justify: center);
     background: $GREY;
     border: none;
     border-radius: 100%;
     height: grid(6);
     width: grid(6);
+
+    .»open & {
+      @include Show;
+    }
+  }
+
+  .results {
+    background: $WHITE;
+    border: 1px solid $GREY;
+    border-top: none;
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
+    display: none;
+    overflow: auto;
+    position: absolute;
+    height: 300px;
+    top: 100%;
+    width: 100%;
+
+    .»open & {
+      display: block;
+    }
   }
 </style>
